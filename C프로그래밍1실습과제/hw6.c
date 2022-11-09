e1dfa9aa416da66b256d045d0d727e0c2682125c
희선 <101498678+Hamzzi17@users.noreply.github.com>
2022-11-09 14:46:43 +0900

    Create hw6.c
    9주차 개념 확인 과제

---
+++ C프로그래밍1실습과제/hw6.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int Odd(int X)
+{
+	if (X % 2 == 1)
+	{
+		printf("%d ", X);
+	}
+	return 0;
+}
+
+int Even(int Y)
+{
+	if (Y % 2 == 0)
+	{
+		printf("%d ", Y);
+	}
+	return 0;
+}
+
+int main(void)
+{
+	int Array[5];
+	int i;
+
+	printf("Please input five integers: ");
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &Array[i]);
+	}
+
+	printf("\nOdd numbers: ");
+	for (int i = 0; i < 5; i++)
+	{
+		Odd(Array[i]);
+	}
+	printf("\nEven numbers: ");
+	for (int i = 0; i < 5; i++)
+	{
+		Even(Array[i]);
+	}
+	return 0;
+}
